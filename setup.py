95fbdbc6ef60e899810b8915ca8ed9dd1d12da68
Felipe Endlich <endlichfelipe@gmail.com>
2021-07-18 12:12:34 -0300

    fix version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='py-mapper',
-    version='0.1.0',
+    version='0.1.0a',
     author='Felipe Endlich',
     author_email='endlichfelipe@gmail.com',
     description='Python mapper library',
